599c3adf44e1d741e46aebb4b1e3b2cd4191b1fd
kwikiel <random8902@gmail.com>
2015-01-27 19:39:39 -0500

    pep 8 passed

--- app/views.py
+++ app/views.py
@@ -2,34 +2,24 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
-
 def index():
-    
-    posts = [
-            {'author': {'nickname': 'John'},
-             'body' : 'Such a day'
-                }
-    ]
+    posts = [{'author': {'nickname': 'John'}, 'body': 'Such a day'}]
+    user = {'nickname': 'Kacper'}
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-    user = {'nickname': 'Kacper'} #mockup
-    return render_template('index.html',
-                           title='Home',
-                           user = user,
-                           posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me="%s"' % 
-                (form.openid.data, str(form.remember_me.data)))
+        flash('Login requested for OpenID="%s", remember_me="%s"' %
+              (form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
 
     return render_template('login.html',
                            title='Sign In',
                            form=form,
-                           providers =app.config['OPENID_PROVIDERS'])
-
+                           providers=app.config['OPENID_PROVIDERS'])
